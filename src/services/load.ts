862d0037ca0ec7ca9a4a226f2bb6bd58105a6a47
khertg <kgeverola@alumni.passerellesnumeriques.org>
2021-06-08 14:32:54 +0800

    Add auth header to load service

--- src/services/load.ts
+++ src/services/load.ts
@@ -1,39 +1,70 @@
 import axios from './custom-axios';
 import { IPagedLoad, ILoad } from '../models/load';
+import { getAuthHeader } from '../helpers/auth-common';
 
 const url = `${process.env.REACT_APP_API_URL}/api/v1/load`;
 
 export async function getList(queryString?: string) {
+  const config = {
+    headers: {
+      ...getAuthHeader(),
+    },
+  };
+
   return await axios
-    .get<{ result: IPagedLoad }>(url + (queryString ? queryString : ''))
+    .get<{ result: IPagedLoad }>(url + (queryString ? queryString : ''), config)
     .then((response) => {
       return response.data.result;
     });
 }
 
 export async function create(data: any) {
-  return await axios.post<{ result: ILoad }>(url, data).then((response) => {
-    return response.data.result;
-  });
+  const config = {
+    headers: {
+      ...getAuthHeader(),
+    },
+  };
+  return await axios
+    .post<{ result: ILoad }>(url, data, config)
+    .then((response) => {
+      return response.data.result;
+    });
 }
 
 export async function edit(id: number, data: any) {
+  const config = {
+    headers: {
+      ...getAuthHeader(),
+    },
+  };
   return await axios
-    .patch<{ result: ILoad }>(`${url}/${id}`, data)
+    .patch<{ result: ILoad }>(`${url}/${id}`, data, config)
     .then((response) => {
       return response.data.result;
     });
 }
 
 export async function get(id: string) {
-  return await axios.get<{ result: ILoad }>(`${url}/${id}`).then((response) => {
-    return response.data.result;
-  });
+  const config = {
+    headers: {
+      ...getAuthHeader(),
+    },
+  };
+  return await axios
+    .get<{ result: ILoad }>(`${url}/${id}`, config)
+    .then((response) => {
+      return response.data.result;
+    });
 }
 
 export async function deleteById(id: number) {
+  const config = {
+    headers: {
+      ...getAuthHeader(),
+    },
+  };
   return await axios
-    .delete<{ result: ILoad }>(`${url}/${id}`)
+    .delete<{ result: ILoad }>(`${url}/${id}`, config)
     .then((response) => {
       return response.data.result;
     });
